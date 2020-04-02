745b11e75cf3fe5e1ded912159dca7b5a03bfdef
BabininOleg2020 <62940751+BabininOleg2020@users.noreply.github.com>
2020-04-02 20:29:22 +0300

    Выполнил 4 и 5 лабу
    Переопределил метод runnable и запустил вычисления в другом потоке, а так же организовал логирование

--- Solution_norm/src/com/company/Main.java
+++ Solution_norm/src/com/company/Main.java
@@ -1,15 +1,18 @@
 package com.company;
+import java.io.IOException;
 import java.util.*;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+import static java.util.logging.Level.INFO;
+import static java.util.logging.Level.WARNING;
 
 public class Main {
     private static final Scanner scanner = new Scanner(System.in);
+    private static Logger myLogger = Logger.getLogger("com.company.Main");
 
     /*
-    Дано: n - количество чисел в массиве
-    *ar - массив чисел
-    * 0 < n < 100
-    *-100 <= ar[i] <= 100
-    *   вычислить нормализованное количество положительных, отрицательных и нулевых элементов
     * Пример:
     *6
     *-4 3 -9 0 4 1
@@ -19,21 +22,54 @@ public class Main {
     *0.333333
     *0.166667
      */
-    
-    public static void main(String[] args) {
-        System.out.println("Enter kol-vo n:");
-        int n = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        int[] arr = new int[n];
-        System.out.println("Enter element mass:");
-        String[] arrItems = scanner.nextLine().split(" ");
-        System.out.println("Press Enter");
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-        scanner.close();
-        Solution solution = new Solution();
-        solution.setData(arrItems);
-        solution.answer();
+    public static void main(String[] args) throws IOException {
+        ConsoleHandler consoleHandler = new ConsoleHandler();
+        consoleHandler.setLevel(INFO);
+        consoleHandler.setFormatter(new SimpleFormatter());
 
+
+        try {
+
+            myLogger.log(INFO,"MAIN THREAD : PRINT FOR USER");
+            System.out.println("Enter kol-vo n:");
+            myLogger.log(INFO, "MAIN THREAD : \n" +
+                    "INITIALIZATION 'N' ");
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+            myLogger.log(INFO, "MAIN THREAD : \n" +
+                    "INITIALIZATION 'ARR' ");
+            int[] arr = new int[n];
+            System.out.println("Enter element mass:");
+            myLogger.log(INFO, "MAIN THREAD : \n" +
+                    "INITIALIZATION SCANNER ");
+            final String[] arrItems = scanner.nextLine().split(" ");
+            System.out.println("Press Enter");
+            myLogger.log(INFO, "MAIN THREAD : \n" +
+                    "SCANNER SKIP ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+            myLogger.log(INFO, "MAIN THREAD : \n" +
+                    "SCANNER CLOSE ");
+            scanner.close();
+            myLogger.log(INFO, "MAIN THREAD : \n" +
+                    "OVVERIDE RUNNABLE ");
+            Runnable task = new Runnable() {
+                @Override
+                public void run() {
+                    Solution solution = new Solution();
+                    solution.setData(arrItems);
+                    solution.answer();
+                }
+            };
+            myLogger.log(INFO, "MAIN THREAD : \n" +
+                    "INITIALIZATION THREAD ");
+            Thread myThread = new Thread(task);
+            myLogger.log(INFO, "MAIN THREAD : \n" +
+                    "THREAD START ");
+            myThread.start();
+
+        }catch (Exception e){
+            myLogger.log(WARNING,"SOMETHING WRONG",e);
+        }
     }
 }
